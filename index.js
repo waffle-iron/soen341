d71bef43bcb1ca6a4fb90282323cc11e23acc59e
Earthii <xiao_eric@hotmail.com>
2017-01-30 23:50:40 -0500

    bower.json and prototype of conversation between person completed

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//serve public folders, potentially Angular App
+app.use(express.static(__dirname + '/public'));
+
+//serve bower components
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',function(socket){
+    //user is connected...
+    console.log('a user connected');
+
+    //when user sends a message
+    socket.on('chat message',function(msg){
+       console.log('message sent: ' + msg);
+       io.emit('chat message',msg);
+    });
+
+    //when user leaves, do something
+    socket.on('disconnect', function(){
+       console.log('user disconnected')
+    });
+});
+
+http.listen(3000,function(){
+    console.log('listening on*: 3000');
+});
