34d64ceed38c2a6d1620326f1991d7e6f1868f63
monkeytheifx <digigundam@hotmail.com>
2017-02-01 02:36:15 -0500

    add route to login page and add folder for temp db

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var db = require('./temp_database/temp_userDB')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 //serve public folders, potentially Angular App
 app.use(express.static(__dirname + '/public/'));
@@ -9,9 +13,33 @@ app.use(express.static(__dirname + '/public/'));
 //serve bower components
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.get('/chat', function(req,res){
-   res.sendFile(__dirname + '/public/views/index.html');
+
+//routings
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/public/views/login.html');
 });
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.username);
+    for(var i =0; i< db.users.length; i++) {
+        if (req.body.username == db.users[i].username && req.body.password == db.users[i].password) {
+            res.redirect('/chat');
+        } else {
+            res.redirect('/');
+            res.send('Incorrect username or password');
+        }
+    }
+});
+
+app.get('/chat', function(req,res){
+    res.sendFile(__dirname + '/public/views/chat.html');
+});
+
+
+
+
+
 
 io.on('connection',function(socket){
     //user is connected...
@@ -19,13 +47,13 @@ io.on('connection',function(socket){
 
     //when user sends a message
     socket.on('chat message',function(msg){
-       console.log('message sent: ' + msg);
-       io.emit('chat message',msg);
+        console.log('message sent: ' + msg);
+        io.emit('chat message',msg);
     });
 
     //when user leaves, do something
     socket.on('disconnect', function(){
-       console.log('user disconnected')
+        console.log('user disconnected')
     });
 });
 
--- public/javascript/login_authentication.js
+++ public/javascript/login_authentication.js
@@ -1,16 +1,19 @@
-var myApp = angular.module("formApp",[]);
-
-myApp.controller("mainController", function(){
-    var vm = this;
-    vm.submit = function(givenName, givenPass) {
-        var username = givenName;
-        var password = givenPass;
-        console.log("hi");
-        if(username == 'admin' && password == 'admin'){
-            alert('Information valid');
-        }
-        else{
-            alert('Incorrect information');
-        }
-    };
-})
+// var myApp = angular.module("formApp",[]);
+// var db = require('../../temp_userDB.json');
+//
+// myApp.controller("mainController", function(){
+//     var vm = this;
+//     vm.submit = function(givenName, givenPass) {
+//         var username = givenName;
+//         var password = givenPass;
+//
+//         for(var i =0; i< db.users.length; i++){
+//             if(username == db.users[i].username && password == db.users[i].password){
+//                 alert('Information valid');
+//             }
+//             else{
+//                 alert('Incorrect information');
+//             }
+//         }
+//     };
+// });
